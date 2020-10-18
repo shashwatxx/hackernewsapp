cdd618e0ce500d4482e14bb77401ef6b9289cdc9
shashwatxx <shashwatmishra2111@gmail.com>
2020-10-18 09:26:19 +0530

    stasrting more on built_value

--- lib/json_parsing.dart
+++ lib/json_parsing.dart
@@ -1,34 +1,31 @@
 import 'package:built_value/built_value.dart';
+import 'package:built_collection/built_collection.dart';
 
 part 'json_parsing.g.dart';
 
 abstract class Article implements Built<Article, ArticleBuilder> {
   int get id;
 
+deleted
+type
+by
+time
+text
+dead
+parent
+poll
+kids  
+url
+score
+title
+parts
+descendants
   Article._();
   factory Article([void Function(ArticleBuilder) updates]) = _$Article;
 }
 
-//class Article {
-//  final String text;
-//  final String url;
-//  final String by;
-//  final int time;
-//  final int score;
-//
-//  const Article({this.text, this.url, this.by, this.time, this.score, s});
-//
-//  factory Article.fromJson(Map<String, dynamic> json) {
-//    if (json == null) return null;
-//    return Article(
-//      text: json['text'] ?? '[null]',
-//      url: json['url'],
-//      time: json['age'] ?? 0,
-//      by: json['by'],
-//      score: json['score'],
-//    );
-//  }
-//}
+
+
 
 List<int> parseTopStories(String jsonStr) {
 //  return List<int>();
