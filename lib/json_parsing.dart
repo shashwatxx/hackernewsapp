298a82973036e69c79707783e8127b55c81b81eb
shashwatxx <shashwatmishra2111@gmail.com>
2020-10-19 05:04:03 +0530

    Data fetcjing done

--- lib/json_parsing.dart
+++ lib/json_parsing.dart
@@ -1,72 +1,72 @@
-import 'dart:convert';
+// import 'dart:convert';
 
-import 'package:built_collection/built_collection.dart';
-import 'package:built_value/built_value.dart';
-import 'package:built_value/serializer.dart';
+// import 'package:built_collection/built_collection.dart';
+// import 'package:built_value/built_value.dart';
+// import 'package:built_value/serializer.dart';
 
-import 'serializers.dart';
+// import 'serializers.dart';
 
-part 'json_parsing.g.dart';
+// part 'json_parsing.g.dart';
 
-abstract class Article implements Built<Article, ArticleBuilder> {
-  static Serializer<Article> get serializer => _$articleSerializer;
+// abstract class Article implements Built<Article, ArticleBuilder> {
+//   static Serializer<Article> get serializer => _$articleSerializer;
 
-  int get id;
+//   int get id;
 
-  @nullable
-  bool get deleted;
+//   @nullable
+//   bool get deleted;
 
-  // @nullable
-  String get type;
+//   // @nullable
+//   String get type;
 
-  // @nullable
-  String get by;
+//   // @nullable
+//   String get by;
 
-  int get time;
+//   int get time;
 
-  @nullable
-  String get text;
+//   @nullable
+//   String get text;
 
-  @nullable
-  bool get dead;
+//   @nullable
+//   bool get dead;
 
-  @nullable
-  int get parent;
+//   @nullable
+//   int get parent;
 
-  @nullable
-  int get poll;
+//   @nullable
+//   int get poll;
 
-  BuiltList<int> get kids;
+//   BuiltList<int> get kids;
 
-  @nullable
-  String get url;
+//   @nullable
+//   String get url;
 
-  @nullable
-  int get score;
+//   @nullable
+//   int get score;
 
-  String get title;
+//   String get title;
 
-  BuiltList<int> get parts;
+//   BuiltList<int> get parts;
 
-  @nullable
-  int get descendants;
+//   @nullable
+//   int get descendants;
 
-  Article._();
-  factory Article([void Function(ArticleBuilder) updates]) = _$Article;
-}
+//   Article._();
+//   factory Article([void Function(ArticleBuilder) updates]) = _$Article;
+// }
 
-List<int> parseTopStories(String jsonStr) {
-//  return List<int>();
-  return [];
-//  final parsed = jsonDecode(jsonStr);
-//  final listOFIds = List<int>.from(parsed);
-//  return listOFIds;
-}
+// List<int> parseTopStories(String jsonStr) {
+// //  return List<int>();
+//   return [];
+// //  final parsed = jsonDecode(jsonStr);
+// //  final listOFIds = List<int>.from(parsed);
+// //  return listOFIds;
+// }
 
-Article parseArticle(String jsonStr) {
-  final parsed = jsonDecode(jsonStr);
-  Article article =
-      standardSerializers.deserializeWith(Article.serializer, parsed);
-  //  Article.fromJson(parsed);
-  return article;
-}
+// Article parseArticle(String jsonStr) {
+//   final parsed = jsonDecode(jsonStr);
+//   Article article =
+//       standardSerializers.deserializeWith(Article.serializer, parsed);
+//   //  Article.fromJson(parsed);
+//   return article;
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hackernewsapp/src/article.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
@@ -30,7 +31,30 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<Article> _articles = articles;
+  // List<Article> _articles = []; //articles;
+  List<int> _ids = [
+    24799660,
+    24789865,
+    24817304,
+    24798302,
+    24791357,
+    24790055,
+    24800037,
+    24795375,
+    24797423,
+    24808071,
+    24793170,
+    24813795,
+    24814687,
+  ];
+
+  Future<Article> _getArticle(int id) async {
+    final storyUrl = 'https://hacker-news.firebaseio.com/v0/item/$id.json';
+    final storyRes = await http.get(storyUrl);
+    if (storyRes.statusCode == 200) {
+      return parseArticle(storyRes.body);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,43 +62,44 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: RefreshIndicator(
-          onRefresh: () async {
-            // Scaffold.of(context)
-            //     .showSnackBar(SnackBar(content: Text("Refreshed!!!")));
-            await Future.delayed(Duration(seconds: 1));
-            setState(() {
-              _articles.removeAt(0);
-            });
-          },
-          child: ListView(
-            children: _articles.map(_buildItem).toList(),
-          ),
-        ),
-      ),
+      body: ListView(
+          children: _ids
+              .map((i) => FutureBuilder<Article>(
+                  future: _getArticle(i),
+                  builder:
+                      (BuildContext context, AsyncSnapshot<Article> snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      return _buildItem(snapshot.data);
+                      // return Padding(
+                      //   padding: const EdgeInsets.all(8.0),
+                      //   child: Text(snapshot.data.title),
+                      // );
+                    } else {
+                      return Center(child: CircularProgressIndicator());
+                    }
+                  }))
+              .toList()),
     );
   }
 
   Widget _buildItem(Article article) {
     return Padding(
-      key: Key(article.text),
+      key: Key(article.title),
       padding: const EdgeInsets.all(16.0),
       child: ExpansionTile(
         title: Text(
-          article.text,
+          article.title,
           style: TextStyle(fontSize: 20),
         ),
         children: [
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Text("${article.commentsCount} Comments"),
+              Text("${article.type}"),
               IconButton(
                   onPressed: () async {
-                    String fakeUrl = article.url.toString();
-                    if (await canLaunch(fakeUrl)) {
-                      launch(fakeUrl);
+                    if (await canLaunch(article.url)) {
+                      launch(article.url);
                     }
                   },
                   icon: Icon(Icons.launch))
