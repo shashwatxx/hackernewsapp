a78c6dcf7a4b8529ab89b4531ded5e019c84ff1c
shashwatxx <shashwatmishra2111@gmail.com>
2020-10-18 12:32:40 +0530

    auto

--- lib/json_parsing.dart
+++ lib/json_parsing.dart
@@ -1,31 +1,34 @@
+import 'dart:convert';
+
 import 'package:built_value/built_value.dart';
 import 'package:built_collection/built_collection.dart';
+import 'package:built_value/serializer.dart';
+import 'serializers.dart';
 
 part 'json_parsing.g.dart';
 
 abstract class Article implements Built<Article, ArticleBuilder> {
-  int get id;
+  static Serializer<Article> get serializer => _$articleSerializer;
 
-deleted
-type
-by
-time
-text
-dead
-parent
-poll
-kids  
-url
-score
-title
-parts
-descendants
+  int get id;
+  bool get deleted;
+  String get type;
+  String get by;
+  int get time;
+  String get text;
+  bool get dead;
+  int get parent;
+  int get poll;
+  BuiltList<int> get kids;
+  String get url;
+  int get score;
+  String get title;
+  BuiltList<int> get parts;
+  int get descendants;
+
   Article._();
   factory Article([void Function(ArticleBuilder) updates]) = _$Article;
 }
-
-
-
 
 List<int> parseTopStories(String jsonStr) {
 //  return List<int>();
@@ -36,8 +39,8 @@ List<int> parseTopStories(String jsonStr) {
 }
 
 Article parseArticle(String jsonStr) {
-  return null;
-//  final parsed = jsonDecode(jsonStr);
-//  Article article = Article.fromJson(parsed);
-//  return article;
+  final parsed = jsonDecode(jsonStr);
+  Article article = serializers.deserializeWith(Article.serializer, parsed);
+  //  Article.fromJson(parsed);
+  return article;
 }
